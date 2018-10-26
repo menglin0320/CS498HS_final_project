ddca2915d62ec99a5798afb3d99771b589952e93
menglin0320 <1372920944@qq.com>
2018-10-26 15:23:09 -0400

    debugging train

--- src/get_batches.py
+++ src/get_batches.py
@@ -1,37 +1,40 @@
 import numpy as np
 
+
 def padding_samples(passage_embedding, max_n_embed):
     padding_needed = max_n_embed - len(passage_embedding)
     print('max_n_embed{}, passage_embedding{}'.format(max_n_embed, len(passage_embedding)))
     if padding_needed < 0:
-        raise(ValueError('max embedding length evaluate fault'))
+        raise (ValueError('max embedding length evaluate fault'))
     for i in range(0, padding_needed):
-        passage_embedding.append([0]*300)
+        passage_embedding.append([0] * 300)
     return passage_embedding
+
 
 def cvt2onehot(values):
     n_values = np.max(values) + 1
     ret = np.eye(n_values)[values]
     return ret
 
+
 def get_batches(data_, batch_size):
     ret = []
     i = 0
-    while(batch_size*i < len(data_)):
+    while (batch_size * i < len(data_)):
         cur_batch = {}
-        cur_batch_start = i*batch_size
-        cur_batch_size = max(batch_size, len(data_) - i*batch_size)
+        cur_batch_start = i * batch_size
+        cur_batch_size = min(batch_size, len(data_) - i * batch_size)
         max_n_embed = len(data_[cur_batch_start + cur_batch_size - 1]['attributes'])
         cur_batch['data'] = []
         cur_batch['label'] = []
         cur_batch['mask'] = []
         for data_ind in range(cur_batch_start, cur_batch_start + cur_batch_size):
-            cur_batch['mask'].append(len(data_[data_ind]['attributes'])-1)
+            cur_batch['mask'].append(len(data_[data_ind]['attributes']) - 1)
             cur_batch['data'].append(padding_samples(data_[data_ind]['attributes'], max_n_embed))
             cur_batch['label'].append(data_[data_ind]['label'])
         cur_batch['mask'] = cvt2onehot(np.asarray(cur_batch['mask']))
         cur_batch['label'] = np.asarray(cur_batch['label'])
         cur_batch['data'] = np.asarray(cur_batch['data'])
         ret.append(cur_batch)
-        i+=1
+        i += 1
     return ret
