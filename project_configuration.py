1eb471eff10a1bbb402947c58c8ec5505df379c2
menglin0320 <1372920944@qq.com>
2018-10-25 21:03:44 -0400

    added data

--- project_configuration.py
+++ project_configuration.py
@@ -1,12 +1,6 @@
 class config:
-    sample_path = 'C:/Users/MyLappy/CS 498/.spyproject/train_data.txt'
-    label_path = 'C:/Users/MyLappy/CS 498/.spyproject/test_data.txt'
-
-    raw_data_path = 'data/raw_data/Accepted_answer_prediction_data_train.txt'
-    raw_label_path = 'data/raw_data/Accepted_answer_prediction_labels_train.txt'
-
-    preprocessed_data_path = ''
-    preprocessed_label_path = ''
+    label_path = 'data/processed_data/labels.txt'
+    processed_data_path = 'data/processed_data/processed_attributes.txt'
     word_embedding_json_path = 'resource/word_embeddings.json'
     word_frequence_json = 'resource/word_frequence.json'
     data_path = 'data/all_data.json'
