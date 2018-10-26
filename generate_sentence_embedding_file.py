8ce9114f6635f6afce541d1099417f51c0946dbc
menglin0320 <1372920944@qq.com>
2018-10-25 20:57:28 -0400

    added data

---
+++ generate_sentence_embedding_file.py
@@ -0,0 +1,11 @@
+from project_configuration import config
+
+
+def generate_sentence_embedding_files(in_path, out_path):
+
+
+if __name__ == 'main':
+    #
+    in_data_path = config.processed_data_path
+    out_data_path = config.sentence_embedding_json_path
+    #
--- project_configuration.py
+++ project_configuration.py
@@ -1,4 +1,7 @@
 class config:
+    sample_path = 'C:/Users/MyLappy/CS 498/.spyproject/train_data.txt'
+    label_path = 'C:/Users/MyLappy/CS 498/.spyproject/test_data.txt'
+
     raw_data_path = 'data/raw_data/Accepted_answer_prediction_data_train.txt'
     raw_label_path = 'data/raw_data/Accepted_answer_prediction_labels_train.txt'
 
