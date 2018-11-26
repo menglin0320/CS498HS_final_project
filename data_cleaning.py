c31081ead5152e8083d648955086f983991af3d0
jasonwang1019 <b02901019@ntu.edu.tw>
2018-11-25 22:19:15 -0700

    add new properties and change data format

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,6 +3,7 @@ import pickle
 from bs4 import BeautifulSoup
 from nltk import tokenize, word_tokenize
 import os
+from datetime import datetime
 
 # abbriviation_replace to avoid wrong sentence slicing (due to misinterpreted meaning of '.')
 def abbriviation_replace(sentence):
@@ -12,6 +13,22 @@ def abbriviation_replace(sentence):
         sentence = sentence.replace(key, abbreviations[key])
     return sentence
 
+def str2timestamp(str):
+    return datetime.strptime(str, '%Y-%m-%dT%H:%M:%S.%f').timestamp()
+
+def getDate(obj):
+    if obj["CreationDate"]:
+        return str2timestamp(obj["CreationDate"])
+    elif obj["LastEditDate"]:
+        return str2timestamp(obj["LastEditDate"])
+    elif obj["LastActivityDate"]:
+        return str2timestamp(obj["LastActivityDate"])
+    elif obj["CommunityOwnedDate"]:
+        return str2timestamp(obj["CommunityOwnedDate"])
+    elif obj["ClosedDate"]:
+        return str2timestamp(obj["ClosedDate"])
+    else:
+        return -1
 
 # get embedding table
 if __name__ == '__main__':
@@ -33,7 +50,7 @@ if __name__ == '__main__':
 
     # change Posts.xml into List of data records (saved in data_pkl) and also output a word_pkl file.
     # word_pkl contains a Dictionary having word(string object) as key and vector(List of floats) as value. It is also a subset of glove.42B.300d.txt
-    # Data record format: [Body(string), accepted tag(bool)]
+    # Data record format: [Body(string), accepted tag(bool), timestamp(float), score(int), commentCount(int)]
 
     obj = untangle.parse('Posts.xml')
     questionDict = {}  # questionDict[id] = false ,if this question has not choose accepted answer
@@ -49,6 +66,11 @@ if __name__ == '__main__':
     answer_count = 0
     non_matched_num = 0
     non_matched = {}
+    question_with_date = 0
+    answer_with_date = 0
+    both_with_date = 0
+    answer_with_score = 0
+    answer_with_commentCount = 0
 
     for i in range(len(obj.posts.row)):
         # progress bar
@@ -58,9 +80,11 @@ if __name__ == '__main__':
         # question type post
         if obj.posts.row[i]['PostTypeId'] == '1':
             question_count += 1
-            questionDict[obj.posts.row[i]['Id']] = bool(obj.posts.row[i]['AcceptedAnswerId'])
+            questionDict[obj.posts.row[i]['Id']] = [bool(obj.posts.row[i]['AcceptedAnswerId']), getDate(obj.posts.row[i])]
+            if obj.posts.row[i]["CreationDate"]:
+                question_with_date += 1
             if bool(obj.posts.row[i]['AcceptedAnswerId']):
-                questionDict[obj.posts.row[i]['Id']] = obj.posts.row[i]['AcceptedAnswerId']
+                questionDict[obj.posts.row[i]['Id']][0] = obj.posts.row[i]['AcceptedAnswerId']
         # answer type post
         else:
             answer_count += 1
@@ -68,7 +92,7 @@ if __name__ == '__main__':
             if obj.posts.row[i]['ParentId'] not in questionDict:
                 weird_ans_count += 1
             # parent question has choose accepted answer
-            elif questionDict[obj.posts.row[i]['ParentId']]:
+            elif questionDict[obj.posts.row[i]['ParentId']][0]:
                 # data cleaning
                 raw = BeautifulSoup(obj.posts.row[i]['Body'], "lxml").get_text()
                 raw = raw.lower()
@@ -93,12 +117,28 @@ if __name__ == '__main__':
                                 non_matched[w] = 1
                     raw += [" ".join(ok_words)]
                 raw = " ".join(raw)
-                acc = (questionDict[obj.posts.row[i]['ParentId']] == obj.posts.row[i]['Id'])
+                acc = (questionDict[obj.posts.row[i]['ParentId']][0] == obj.posts.row[i]['Id'])
+                score = -1
+                commentCount = -1
+                if obj.posts.row[i]["Score"]:
+                    score = int(obj.posts.row[i]["Score"])
+                    answer_with_score += 1
+                if obj.posts.row[i]["CommentCount"]:
+                    commentCount = int(obj.posts.row[i]["CommentCount"])
+                    answer_with_commentCount += 1
+                ansCreationDate = getDate(obj.posts.row[i])
+                quesCreationDate = questionDict[obj.posts.row[i]['ParentId']][1]
+                timestamp = -1
+                if obj.posts.row[i]["CreationDate"]:
+                    answer_with_date += 1
+                if ansCreationDate != -1 and quesCreationDate != -1:
+                    both_with_date += 1
+                    timestamp = ansCreationDate - quesCreationDate
                 # statistic
                 acc_count += acc
                 not_acc_count += 1 - acc
                 # define data record format here
-                answers += [[raw, acc]]
+                answers += [[raw, acc, timestamp, score, commentCount]]
             # parent question has not choose accepted answer
             else:
                 not_rate_count += 1
@@ -126,6 +166,8 @@ if __name__ == '__main__':
             len(questionDict), rated_question, len(obj.posts.row) - len(questionDict), acc_count, not_acc_count,
             not_rate_count, weird_ans_count))
     print("match failed words number:{}, kinds:{}".format(non_matched_num, len(non_matched)))
+    print("with date: Q:{} A:{} Both:{}".format(question_with_date, answer_with_date, both_with_date))
+    print("Ans with: score:{} commentCount:{}".format(answer_with_score, answer_with_commentCount))
     print(answers[0])
     # output files
     current_dir = os.path.dirname(os.path.abspath(__file__))
